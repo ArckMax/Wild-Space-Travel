00e342999477dbca6cef8b5fabc098550dea8fef
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2018-11-16 16:41:31 +0100

    fix img location + details before deploy

--- src/app/trend/trend.component.ts
+++ src/app/trend/trend.component.ts
@@ -10,7 +10,7 @@ declare var TweenMax:any;
 export class TrendComponent implements OnInit {
 
   title1:string = "The Marsupial Lake is a must-see on the Road of Happiness. If the purity of the sky makes this place ideal for star-watching, you will also be able to enjoy many other different activities during daytime. For example, dive in the turquoise-blue water of the lake, or enjoy the breath-taking landscapes that make a perfect scenery for hiking or romantic picnics.";
-  title2:string = "You are travelling for business purposes, this planet offers all comfort you will need: meeting room for virtual conferences, automatised desk concierge, and many more services. Located very close from the most influent companies of the galaxy, you will conduct your business in the best conditions.";
+  title2:string = "You are travelling for business purposes, this planet offers all the comfort you will need: meeting room for virtual conferences, automatized desk concierge, and many more services. Located very close from the most influent companies of the galaxy, you will conduct your business in the best conditions.";
   title3:string = "Are you part of the very privileged circle of fortunate people, don't hesitate to travel to Agorra: its decadent lifestyle has been making it famous for decades, Don't be surprised if you don't want to leave ! ";
 
   viewImage1:boolean = false;
